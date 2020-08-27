0fdb8da9de57e34260df0f45b459a22c984fe57c
alexeyBal <alexey.balushkin@feedvisor.com>
2020-08-27 22:03:10 +0300

    react-redux skeleton plus babel, webpack, eslint, stylus support, etc

---
+++ src/app/modules/app/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/App.styl';
+function App() {
+  return (
+    <div className="container">
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Modules from './modules.jsx';
+
+Modules({
+    env: 'dev',
+});
