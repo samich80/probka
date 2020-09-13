45eb39ff31b03f7e4601fc99bec4bf9136e4e0f8
alexeyBal <alexey.balushkin@feedvisor.com>
2020-09-13 18:21:15 +0300

    Move to symfony (env variables)

--- src/Core/Service/StoreService.php
+++ src/Core/Service/StoreService.php
@@ -2,11 +2,33 @@
 
 namespace App\Core\Service;
 
+use Exception;
+
 class StoreService
 {
+    private int $storeId;
+
+    /**
+     * @return int
+     * @throws Exception
+     */
     public function getStoreId(): int
     {
-        // @TODO add logic of getting storeId from env
-        return 1;
+        if (!$this->storeId) {
+            $this->storeId = (int)getenv('STORE_ID');
+            if (!$this->storeId) {
+                throw new Exception("STORE_ID should be initialized");
+            }
+        }
+        return $this->storeId;
+    }
+
+    /**
+     * @required
+     * @param int $storeId
+     */
+    public function setStoreId(int $storeId = 0): void
+    {
+        $this->storeId = $storeId;
     }
 }
